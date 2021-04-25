a08631f9cee7a0b1ab81ac22abb73f71d9b1b451
T-oga331 <81658428+T-oga331@users.noreply.github.com>
2021-04-25 13:26:40 +0900

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,25 +7,35 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // 要素の個数が5、各要素の値を順番に表示
+        // 要素の個数が5、各要素の値を順番と逆順に表示
         // 要素数5の配列を初期化
         int[] grade = new int[5];
 
-        // 順番の各要素に値を代入
-        grade [0] = 10;
-        grade [1] = 11;
-        grade [2] = 12;
-        grade [3] = 13;
-        grade [4] = 14;
+        // 配列の要素に値を代入
+        grade[0] = 10;
+        grade[1] = 11;
+        grade[2] = 12;
+        grade[3] = 13;
+        grade[4] = 14;
 
-        // 順番に表示されるようにする
-        for (int i = 0; i < 5; i++)
+        // 配列要素の数ぶんだけ繰り返し
+        for (int i = 0; i < 5; i++) 
         {
-            Debug.Log ( grade [i] );
+            
+                // 順番に表示する
+                Debug.Log(grade[i]);
         }
-
+        
+        // 配列の要素の数ぶんだけ繰り返し
+        for (int t = 0; t > 5; t--) 
+        {
+            // 逆順に表示する
+            Debug.Log(grade[t]);
+        }
        
+        
     }
+    
 
     // Update is called once per frame
     void Update()
